01b03e2270fad4b8a5189b665dfd93522cf4c4d9
Heorhii Pashchenko <54940741+GeraPashchenko@users.noreply.github.com>
2020-08-24 17:23:10 +0300

    client add

---
+++ client/config-overrides.js
@@ -0,0 +1,8 @@
+const {override, addWebpackPlugin} = require("customize-cra") ;
+const webpack = require('webpack')
+
+module.exports = override(
+    addWebpackPlugin(new webpack.DefinePlugin({
+        __DEV__: process.env.NODE_ENV !== 'production'
+    }))//
+)
--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const db = require('./services/mongodb');
+const serverPort = process.env.PORT; // server port
 
 require('dotenv').config(); // for reading env file
 require('./middleware/cors.js'); // cors middleware wor cross origin requests
 require('./routs/routs').app(app);
 
-const serverPort = process.env.PORT; // server port
 
 app.listen(serverPort, (err)=>{
     if(err) throw err;
